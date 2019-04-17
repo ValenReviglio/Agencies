a75e4aa4f7756972033ba784ff5de392bf27e957
Valentin Reviglio <vreviglio@ar0fvfy40vchv2j.ml.com>
2019-04-17 14:48:41 -0300

    Todos los puntos implementados. Queda pendiente pulir algunos detalles

--- src/main/java/AgenciesGestor.java
+++ src/main/java/AgenciesGestor.java
@@ -1,9 +1,7 @@
 import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -11,10 +9,11 @@ import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Date;
 
 import static spark.Spark.*;
 
-public class AgenciesService {
+public class AgenciesGestor {
     public static void main(String[] args) {
         get("/agencias", (req, res) -> {
             try {
@@ -22,13 +21,24 @@ public class AgenciesService {
 
                 String siteId = req.queryParams("site_id");
                 String paymentMethod = req.queryParams("payment_method_id");
-                String nearTo = req.queryParams("near_to");
-                String order = req.queryParams("order");
+                String zipCode = req.queryParams("zip_code");
 
-                JsonArray data = readUrl("https://api.mercadolibre.com/sites/" + siteId + "/payment_methods/" + paymentMethod + "/agencies?near_to=" + nearTo);
+                String order = req.queryParams("order");
+                
+                String mapsUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="+zipCode+"&key=AIzaSyBfnGx5oJwDXT_XsdqMsF1moWRH0NQYKr0";
+
+                JsonArray mapResults = (JsonArray) readUrl(mapsUrl);
+                JsonObject location =  (JsonObject) mapResults.get(0);
+                String latitude = location.getAsJsonObject("geometry").getAsJsonObject("location").get("lat").getAsString();
+                String longitude = location.getAsJsonObject("geometry").getAsJsonObject("location").get("lng").getAsString();
+                String agenciesUrl = "https://api.mercadolibre.com/sites/" + siteId + "/payment_methods/" + paymentMethod + "/agencies?near_to=" + latitude+","+longitude;
+
+
+                JsonArray data = (JsonArray) readUrl(agenciesUrl);
                 TypeToken<ArrayList<Agency>> token = new TypeToken<ArrayList<Agency>>() {};
                 ArrayList<Agency> agencias = new Gson().fromJson(data, token.getType());
-                
+                logAcceso(agenciesUrl);
+
                 if (order != null) {
                     setCriterio(order);
                     Operador.ordenarArreglo(agencias);
@@ -39,13 +49,14 @@ public class AgenciesService {
                 return "SUCCESS";
             } catch(IOException e) {
                 e.printStackTrace();
+                return "ERROR";
             }
         });
 
         System.out.println();
     }
 
-    public static JsonArray readUrl(String urlString) throws IOException {
+    public static JsonElement readUrl(String urlString) throws IOException {
         BufferedReader reader = null;
         try {
             URL url = new URL(urlString);
@@ -83,5 +94,24 @@ public class AgenciesService {
         }
         Agency.criterio = Agency.Criterio.AGENCY_CODE;
 
+    }
+
+    public static void logAcceso(String url) {
+        BufferedWriter writer = null;
+        try {
+            File file = new File("./api.log");
+            if(!file.exists()) {
+                file.createNewFile();
+            }
+            writer = new BufferedWriter(new FileWriter(file, true));
+            Date fecha = new Date();
+            String log = fecha.toString() +"\t"+url;
+            System.out.println(log);
+            writer.write(log);
+            writer.newLine();
+            writer.close();
+        } catch(IOException e) {
+            System.out.println("No se pudo crear el archivo de log");
+        }
     }
 }
